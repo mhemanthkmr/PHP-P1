dc92161968682348842ac654e479981c9ffa119e
mhemanthkmr <m.hemanthkmr143@gmail.com>
2021-07-07 07:26:04 +0530

    Frontend Finished

--- login.php
+++ login.php
@@ -7,18 +7,10 @@ include("includes/header.php")
         <div class="col-md-6">
             <div class="card">
                 <div class="card-header">
-                    <h4>Login</h4>
+                    <h4>Registeration Form</h4>
                 </div>
                 <div class="card-body">
-                    <form action="code.php" method="post">
-                        <div class="form-group mb-3">
-                            <label for="">Name</label>
-                            <input type="text" class="form-control" name="name">
-                        </div>
-                        <div class="form-group mb-3">
-                            <label for="">Phone</label>
-                            <input type="text" class="form-control" name="phone">
-                        </div>
+                    <form action="">
                         <div class="form-group mb-3">
                             <label for="">Email</label>
                             <input type="email" class="form-control" name="email">
@@ -28,7 +20,7 @@ include("includes/header.php")
                             <input type="password" class="form-control" name="password">
                         </div>
                         <div class="form-group mb-3">
-                            <button class="btn btn-primary" name="register_btn">Submit</button>
+                            <button class="btn btn-primary">Login</button>
                         </div>
                     </form>
                 </div>
