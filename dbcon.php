648038da201c99653958c59ec7233b2e285e32a1
mhemanthkmr <m.hemanthkmr143@gmail.com>
2021-07-08 05:41:00 +0530

    database connect with php

---
+++ dbcon.php
@@ -0,0 +1,13 @@
+<?php 
+
+$con = mysqli_connect("localhost","mhemanthkmr","hemanth123" ,"users");
+// if($con)
+// {
+//     print_r($con);
+//     echo "Succes";
+// }
+// else 
+// {
+//     echo "Failed";
+// }
+?>
