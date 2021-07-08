6ca323945b260665513f72003d3dd81ea053cf8c
mhemanthkmr <m.hemanthkmr143@gmail.com>
2021-07-08 05:41:12 +0530

    added a card aand body

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php 
 include("includes/header.php")
 ?>
-<h1>Hello World</h1>
+<div class="container m-5 border border-dark">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header border-bottom border-dark">
+                    <h2 class="text-center">Login and Register with Email Verification</h2>
+                </div>
+                <div class="card-body">
+                    <h2 class="text-center">PHP,MySql,Composer</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php 
 include("includes/footer.php")
 ?>
